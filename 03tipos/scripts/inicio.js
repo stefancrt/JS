b948f0cb144b9cd3d51e90b514c72d3ad80a9f67
Stefan <stefancrt22@yahoo.com>
2017-05-18 17:20:31 +0200

    act

--- 03tipos/scripts/inicio.js
+++ 03tipos/scripts/inicio.js
@@ -3,4 +3,11 @@ var sNombre, sApellido;
 sNombre = prompt("Dime tu nombre", "Pepe");
 sApellido = prompt("Dime tu apellido");
 
-alert("Hola "+ sNombre + " " + sApellido);
+
+console.log("Hola "+ sNombre + " " + sApellido)
+console.log("Hola "+ sNombre + " " + sApellido)
+console.log("Hola "+ sNombre + " " + sApellido)
+console.log("Hola "+ sNombre + " " + sApellido)
+console.log("Hola "+ sNombre + " " + sApellido)
+
+alert("Hola "+ sNombre + " " + sApellido)
