b776d79280afb41decd5e15c30b3e33d6e5a6f12
Stefan <stefancrt22@yahoo.com>
2017-05-18 21:41:52 +0200

    last

---
+++ 07ambitos/scripts/inicio.js
@@ -0,0 +1,26 @@
+
+// Declaracion de una funcion
+//function sumar () {}
+
+var sName = "Funciones"
+function main () {
+    let sName = "Funcion Main"
+    //let nNum1 = Number(prompt("Dime un nÃºmero", 12));
+    //let nNum2 = Number(prompt("Dime otro", 23));
+    //let nNum3 = +prompt("Dime otro", 3);
+    //let nNum4 = +prompt("Dime otro", 13);
+    
+    if (2) {
+        let sName  = "Condicional"
+        console.log(sName);
+    }
+
+    console.log(sName);
+    
+    
+}
+
+// InvocaciÃ³n de la funcion
+main()
+
+console.log(sName);
